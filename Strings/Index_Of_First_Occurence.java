57dc673f6967c2fb14975dd6674d32183056f9d8
VAIBHAV KAUSHAL <143897604+its-vaibhav-kaushal@users.noreply.github.com>
2024-08-23 18:42:30 +0530

    Create Index_Of_First_Occurence.java

---
+++ Strings/Index_Of_First_Occurence.java
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+// ___________________________________________________________________________________________________________________________________________________________:)
+class Solution {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
